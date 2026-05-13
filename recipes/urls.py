7bcf4c3138d736f6566a8140b4a8cc7c0857de78
Amr-Hamdy-Ammar <amrhamdyragab30@gmail.com>
2026-05-13 21:20:25 +0300

    Update urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('all-recipes/', views.recipesList, name='all_recipes'),
     path('recipe/<str:recipe_id>/', views.recipeDetail, name='recipe_detail'),
+    path('api/favorites/', views.list_favorites, name='list_favorites'),
+    path('api/favorites/toggle/', views.toggle_favorite, name='toggle_favorite'),
 ]
